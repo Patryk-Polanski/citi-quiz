cc09e9e2a0ce894b72d8d832ae247c90b1c189e6
Patryk Polanski <pat.polanski@gmail.com>
2024-03-03 21:26:16 +0000

    style the error page

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -1,4 +1,8 @@
-import { useRouteError, isRouteErrorResponse, Link } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+import BackgroundBlob from "./decorative/BackgroundBlob";
+import Button from "./Button";
+import Icon from "./icons/_Icon";
+import { IconNames } from "../types/enums";
 
 type ErrorProps = {
   errorMessage?: string;
@@ -9,22 +13,35 @@ export default function Error({ errorMessage }: ErrorProps) {
   console.error("💥 ~ Error ~ error:", error);
 
   return (
-    <div className="flex min-h-dvh flex-col items-center justify-center gap-3">
-      {isRouteErrorResponse(error) ? (
-        <>
-          <h1>404: Page not found</h1>
-          <small>{error.data}</small>
-        </>
-      ) : (
-        <>
-          <h1>404: Could not find the requested data</h1>
-          <small>
-            {errorMessage ? errorMessage : "Something went wrong..."}
-          </small>
-        </>
-      )}
-      <div className="mt-8">
-        <Link to="/">Back home</Link>
+    <div className=" bg-sky-600  font-comfortaa text-white">
+      <div className="flex min-h-dvh flex-col items-center justify-center gap-3 bg-gradient-to-br from-white/50 to-white/20">
+        {isRouteErrorResponse(error) ? (
+          <>
+            <h1>404: Page not found</h1>
+            <small>{error.data}</small>
+          </>
+        ) : (
+          <>
+            <h1>404: Could not find the requested data</h1>
+            <small>
+              {errorMessage ? errorMessage : "Something went wrong..."}
+            </small>
+          </>
+        )}
+        <div className="mt-8">
+          <Button el="link" href="/">
+            <Icon
+              iconName={IconNames.Chevron}
+              className="h-[18px] w-[18px] rotate-180"
+            />
+            <span className="w-[2px] self-stretch bg-white opacity-60" />
+            <span>Back Home</span>
+          </Button>
+        </div>
+        <BackgroundBlob classes="top-[200px] right-[200px] h-8 w-8" />
+        <BackgroundBlob classes="top-[180px] left-[200px]" />
+        <BackgroundBlob classes="bottom-[180px] right-[200px]" />
+        <BackgroundBlob classes="bottom-[200px] left-[200px] h-8 w-8" />
       </div>
     </div>
   );
