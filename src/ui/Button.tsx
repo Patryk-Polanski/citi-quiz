1b182fbda30054d22dd89af3239f69b873385872
Patryk-Polanski <pat.polanski@gmail.com>
2024-05-11 17:40:58 +0100

    install tailwind-merge package to fix tailwind styles conflicts, apply to Button

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,4 +1,5 @@
 import { type ComponentPropsWithoutRef } from "react";
+import { twMerge } from "tailwind-merge";
 import { Link } from "react-router-dom";
 
 type ButtonProps = {
@@ -13,22 +14,13 @@ type LinkProps = {
 } & ComponentPropsWithoutRef<"a">;
 
 export default function Button(props: ButtonProps | LinkProps) {
-  const defaultClasses =
-    "flex item-center justify-center gap-4 rounded-[20px] bg-gradient-to-br from-white/50 to-white/5 px-4 py-[18px] drop-shadow-xl backdrop-blur transition duration-0 ease-in-out after:absolute after:inset-0 after:rounded-[20px] after:border-2 after:border-white/20 after:transition after:duration-0 hover:after:border-white/60";
+  const classes = twMerge(
+    "flex item-center justify-center gap-4 rounded-[20px] bg-gradient-to-br from-white/50 to-white/5 px-4 py-[18px] drop-shadow-xl backdrop-blur transition duration-0 ease-in-out after:absolute after:inset-0 after:rounded-[20px] after:border-2 after:border-white/20 after:transition after:duration-0 hover:after:border-white/60",
+    props.classes,
+  );
 
   if (props.el === "link")
-    return (
-      <Link
-        className={`${defaultClasses} ${props.classes}`}
-        to={props.href}
-        {...props}
-      ></Link>
-    );
+    return <Link className={classes} to={props.href} {...props}></Link>;
 
-  return (
-    <button
-      className={`${defaultClasses} ${props.classes}`}
-      {...props}
-    ></button>
-  );
+  return <button className={classes} {...props}></button>;
 }
