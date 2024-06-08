ec2db88c2a50f2a171f1a97c1ee2c2207343d14f
Patryk-Polanski <pat.polanski@gmail.com>
2024-06-08 01:05:51 +0100

    Button - add omitStyles prop that allows default styles to be omitted

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -5,19 +5,23 @@ import { Link } from "react-router-dom";
 type ButtonProps = {
   el: "button";
   classes?: string;
+  omitStyles?: boolean;
 } & ComponentPropsWithoutRef<"button">;
 
 type LinkProps = {
   el: "link";
   href: string;
   classes?: string;
+  omitStyles?: boolean;
 } & ComponentPropsWithoutRef<"a">;
 
 export default function Button(props: ButtonProps | LinkProps) {
-  const classes = twMerge(
-    "flex item-center justify-center gap-4 rounded-[20px] bg-gradient-to-br from-white/50 to-white/5 px-4 py-[18px] drop-shadow-xl backdrop-blur transition duration-0 ease-in-out after:absolute after:inset-0 after:rounded-[20px] after:border-2 after:border-white/20 after:transition after:duration-0 hover:after:border-white/60",
-    props.classes,
-  );
+  const classes = !props.omitStyles
+    ? twMerge(
+        "flex item-center justify-center gap-4 rounded-[20px] bg-gradient-to-br from-white/50 to-white/5 px-4 py-[18px] drop-shadow-xl backdrop-blur transition duration-0 ease-in-out after:absolute after:inset-0 after:rounded-[20px] after:border-2 after:border-white/20 after:transition after:duration-0 hover:after:border-white/60",
+        props.classes,
+      )
+    : "";
 
   if (props.el === "link")
     return <Link className={classes} to={props.href} {...props}></Link>;
